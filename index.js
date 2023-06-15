c368e026d743f9ca96ab18f94c262f462255d284
sravankumar <cheripellysravan0411@gmail.com>
2023-06-15 16:04:33 +0000

    1 remain fail

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="main-heading">Super Over League</h1>
+    <div className="middle">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+        className="size"
+        alt="name"
+      ></img>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+        className="size"
+        alt="name"
+      ></img>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
